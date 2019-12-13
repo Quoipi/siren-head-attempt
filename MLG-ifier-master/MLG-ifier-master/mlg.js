1306898d76ba6b55772af2a9f58f9a525cae4b5a
Xaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxa <47873855+Quoipi@users.noreply.github.com>
2019-12-13 19:13:07 +1100

    Update mlg.js

--- MLG-ifier-master/MLG-ifier-master/mlg.js
+++ MLG-ifier-master/MLG-ifier-master/mlg.js
@@ -8,45 +8,11 @@ var MLGchecker = "xXx_mlg_checker_xXx";
 
 // Replace sources of all images
 var images = [
-	"http://i.imgur.com/spEe2uf.jpg",
-	"http://i.imgur.com/hsGl9J0.png",
-	"http://i.imgur.com/SoE7jVi.jpg",
-	"http://i.imgur.com/reZXpqs.jpg",
-	"http://i.imgur.com/8EHkaox.jpg",
-	"http://i.imgur.com/ymU48Db.gif",
-	"http://puu.sh/3ObVc.gif",
-	"http://puu.sh/41mIF.gif",
-	"http://puu.sh/41mMg.gif",
-	"http://puu.sh/41mP0.gif",
-	"http://puu.sh/41mQ5.gif",
-	"http://i.imgur.com/n41tcFI.gif",
-	"http://i.imgur.com/e9unmW9.png",
-	"http://s3-ec.buzzfed.com/static/enhanced/webdr06/2013/6/12/10/enhanced-buzz-16022-1371047559-13.jpg",
-	"http://i.imgur.com/oTxFnjp.png",
-	"http://i.imgur.com/nC9Sluu.gif",
-	"http://i.imgur.com/b87y0Vp.gif",
-	"http://i.imgur.com/QNy5cAF.png",
-	"http://i.imgur.com/op9PWrr.gif",
-	"http://i.imgur.com/5Ro9KS9.gif?1",
-	"http://i.imgur.com/6tzgjkR.png",
-	"http://i.imgur.com/Sm86W97.png?1",
-    "http://i.ytimg.com/vi/N7W_ilTM1_w/maxresdefault.jpg",
-    "http://i.imgur.com/t4vrAy0.png",
-    "http://i.imgur.com/cmsXNAK.jpg",
-    "http://i.imgur.com/lnQKKNY.gif",
-    "http://i.imgur.com/1Fa8BPq.jpg",
-    "http://i.imgur.com/s2RN7HK.jpg",
-    "http://i.imgur.com/3Cn6ug0.png",
-    "http://files.gamebanana.com/img/ico/sprays/538acd5a7838b.gif" // the sanic one
+	"https://i.imgur.com/ETpSXd0.jpg" //powerful siren head
 ];
 
 var sounds = [
     "airhorn.mp3",
-    "If You Know What I Mean.m4a",
-    "DAMN SON WHERED YOU FIND THIS.mp3",
-    "Oh Baby A Triple.mp3",
-    "SMOKE WEEK EVERYDAY.mp3",
-    "SANIC.mp3"
 ];
 
 /*
@@ -69,15 +35,15 @@ var weedColors = [
 // word replacements
 // Penn Treebank POS tags: https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
 var treeBank = {
-	/* Adjectives */ "JJ": ["dank", "MLG", "euphoric", "( ͡° ͜ʖ ͡°)", "bestselling"],
-	/* Adjective, comparative */ "JJR": ["danker", "more MLG", "( ͡° ͜ʖ ͡°)"],
-	/* Adjective, superlative */ "JJS": ["dankinest", "MLG-inest", "( ͡° ͜ʖ ͡°)"],
-	/* Noun, singular */ "NN": ["dankineer", "pineapple", "Gurl", "m8", "m9", "m80", "parody", "subreddit", "meme", "scrub", "mountain dew", "weed", "blunt", "ebola", "fedora", "trilby", "9/11", "( ͡° ͜ʖ ͡°)", "johnbob", "9gag", "9gagger"],
-	/* Noun, plural */ "NNS": ["dankineers", "pineapples", "Gurlz", "m80s", "m89s", "memes", "scrubs", "doritos", "jimmies", "trilbies", "( ͡° ͜ʖ ͡°)", "johbobs", "9gaggers"],
-	/* Proper noun, singular */ "NNP": ["Sp00nr", "Reddit", "Le Reddit Armie", "Snipars", "Vagabonds", "M'lady", "Good Sir", "9/11", "( ͡° ͜ʖ ͡°)", "Milton Dew", "Megatokyo", "Rush Limbaugh"],
-	/* Verb, past tense */ "VBD": ["rekt", "upboated", "rekm8ed", "rustled", "( ͡° ͜ʖ ͡°)", "robbled", "flustered"],
-	/* Verb, non-3rd person singular present */ "VBP": ["transl8", "feel", "upboat", "upvote", "rip", "rekm8", "rustle", "( ͡° ͜ʖ ͡°)", "robble", "fluster"],
-	/* Verb, 3rd person singular present */ "VBZ": ["rekts", "upboats", "rustles", "( ͡° ͜ʖ ͡°)", "robbles", "flusters"]
+	/* Adjectives */ "JJ": ["ominous", "looming", "dark", "powerful"],
+	/* Adjective, comparative */ "JJR": ["more powerful", "scarier"],
+	/* Adjective, superlative */ "JJS": ["scariest", "most ominous"],
+	/* Noun, singular */ "NN": ["siren", "noise", "blood", "bones", "telephone pole"],
+	/* Noun, plural */ "NNS": ["sirens", "mouths", "teeth"],
+	/* Proper noun, singular */ "NNP": ["Siren Head"],
+	/* Verb, past tense */ "VBD": ["eaten", "killed", "perished", "erased"],
+	/* Verb, non-3rd person singular present */ "VBP": ["running"],
+	/* Verb, 3rd person singular present */ "VBZ": ["eating", "feasting"]
 };
 
 
@@ -101,7 +67,7 @@ if ($("#xXx_mlg_checker_xXx").length === 0){
             // 50% chance of changing each word
             if (tag in treeBank && Math.random() < 0.5){
                 if (Math.random() < 0.1){
-                    text = text.replace(word, "xXx_" + word + "_xXx");
+                    text = text.replace(word, "/̵̛̣̫̦̝̮̎̊́̚" + word + "/̵̛̣̫̦̝̮̎̊́̚");
                 }
                 else {
                     var randWord = getRandomElement(treeBank[tag]);
@@ -109,7 +75,7 @@ if ($("#xXx_mlg_checker_xXx").length === 0){
                     // 5% (0.5*0.1) chance of adding xXx_word_xXx to noun
                     if (tag === "NN" || tag === "NNS" || tag === "NNP"){
                         if (Math.random() < 0.05){
-                            randWord = "xXx_" + randWord + "_xXx";
+                            randWord = ".̸̡̼̜̠͇̣̗̞̙̖͓̻̫̉̈́͗̓̐͆̕͝" + randWord + ".̸̡̼̜̠͇̣̗̞̙̖͓̻̫̉̈́͗̓̐͆̕͝";
                         }
                     }
                     text = text.replace(word, randWord);
@@ -190,7 +156,7 @@ function ayylmao(text){
     for (i = 0; i < lines.length; i++) { 
         var word = lines[i];
         if (word.length > 2 && word.substring(word.length-2, word.length)=="ay") {
-            word = word + "y lmao";
+            word = word + "ay";
             text = text.replace(lines[i], word);
         }
     }
@@ -218,21 +184,6 @@ else
     addSound(initRandSoundID, 'sounds/' + randSound);
 playSound(initRandSoundID);
 
-if (!$("#sanic2fast").length)
-    $("body").append("<img id='sanic2fast' src='http://files.gamebanana.com/img/ico/sprays/538acd5a7838b.gif' />");
-
-$("#sanic2fast").css({
-    "position": "fixed",
-    "top": "calc(50% - 128px)",
-    "left": "-256px",
-    "z-index": "10000000"
-});
-
-if (randSound === "SANIC.mp3"){
-    runSanic();
-}
-
-
 // Change background image of body
 $("body").css({
 	"background-image": getRandomElement(images),
@@ -250,8 +201,7 @@ window.setInterval(function(){
             changeSound(initRandSoundID, 'sounds/' + nextRandSound);
 		playSound(initRandSoundID);
         
-        if (nextRandSound === "SANIC.mp3"){
-            runSanic();
+        
         }
 	}
 }, 5000);
